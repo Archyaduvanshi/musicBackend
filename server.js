1433e2e6967c88f89bc46c2d4c666f2135235bc2
Arch yadav <archyaduvanshpathangi@gmail.com>
2026-06-29 23:24:40 +0530

    Use PORT from environment in server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const app = require('./src/app');
 const connectdb = require('./src/db/db');
+const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB before accepting incoming requests.
 connectdb();
 
-app.listen(3000,()=> {
-    console.log("server is running on port 3000")
-})
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`);
+});
